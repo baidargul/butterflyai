a84a0b6b7577acad64cf90f49e9eee95e1b73240
Baidar Gul <baidargul@outlook.com>
2024-12-01 12:23:24 +0500

    added my signature

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,20 @@ import GeneratorPanel from "@/components/GeneratorPanel";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-[100dvh] max-h-full flex justify-center items-start bg-zinc-100 pt-3 sm:pt-6 p-6">
+    <div className="w-full min-h-[100dvh] max-h-full flex justify-center items-start bg-zinc-100 pt-3 sm:pt-6 pb-10 sm:pb-0 p-6 relative">
       <Controls />
+      <div className="absolute flex gap-1 items-center bottom-1 right-1">
+        <span className="font-semibold">Created by:</span>
+        <span>
+          <a
+            className="text-[#3B8FB5] font-bold font-sans"
+            href="https://www.linkedin.com/in/baidargul/"
+            target="_blank"
+          >
+            Baidar Gul
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
