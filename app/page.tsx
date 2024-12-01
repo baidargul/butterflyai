22125d3758fe5ce46998ec5b9cfdebfe807eb092
Baidar Gul <baidargul@outlook.com>
2024-12-01 09:55:10 +0500

    better ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import GeneratorPanel from "@/components/GeneratorPanel";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-[100dvh] max-h-[100vh] flex justify-center items-start bg-zinc-50 p-2 px-6">
+    <div className="w-full min-h-[100dvh] max-h-full flex justify-center items-start bg-zinc-50 p-2 px-6">
       <Controls />
     </div>
   );
--- components/Controls.tsx
+++ components/Controls.tsx
@@ -8,7 +8,33 @@ type Props = {};
 const Controls = (props: Props) => {
   const [images, setImages] = useState<
     { prompt: string; image: string; created_at: string }[]
-  >([]);
+  >([
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/01.jpg", created_at: "" },
+    { prompt: "This is a demo", image: "/images/02.jpg", created_at: "" },
+  ]);
+
+  const handleOldImageClick = (image: {
+    prompt: string;
+    image: string;
+    created_at: string;
+  }) => {
+    //copy prompt to clipboard
+    navigator.clipboard.writeText(image.prompt);
+  };
 
   return (
     <div className="flex flex-col min-h-[100dvh] justify-center items-center">
@@ -16,15 +42,18 @@ const Controls = (props: Props) => {
         <GeneratorPanel setImages={setImages} />
       </div>
       {images.length > 0 && (
-        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-7 lg:grid-cols-8 mt-4 p-2 gap-2 w-full drop-shadow-sm bg-gradient-to-b from-white to-zinc-50 rounded border border-zinc-200 place-items-center justify-center justify-items-center place-content-center">
+        <div className="columns-2 sm:columns-4 lg:columns-8 my-4 p-4 pb-2 gap-2 w-full drop-shadow-sm bg-gradient-to-b from-white to-zinc-50 rounded border border-zinc-200 place-items-center justify-center justify-items-center place-content-center">
           {images.map((image, i) => {
             return (
               <div
                 key={i}
-                className="flex flex-col gap-1 cursor-pointer select-none"
+                className="flex flex-col gap-1 cursor-pointer select-none mb-1"
               >
                 <ImageViewer image={image.image} />
-                <div className="tracking-tight text-sm line-clamp-3">
+                <div
+                  onClick={() => handleOldImageClick(image)}
+                  className="tracking-wide p-1 bg-gradient-to-r from-white to-zinc-50 border border-zinc-200 rounded text-xs line-clamp-3 italic active:scale-95 transition-all duration-100 ease-in-out"
+                >
                   {image.prompt}
                 </div>
               </div>
